9fe1bb9933d5c564538ef27adf343e8bd6b2b096
MariaLundgren <melundgren93@gmail.com>
2021-06-28 20:00:54 +0000

    changed spelling misstake in settings.py and added import os

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 import dj_database_url
 
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_key', 'django-insecure-q7ebwvnpj&(j4zam(s^59wx5t=jy8a_)m8dyp8ak7z7-u278sp'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-q7ebwvnpj&(j4zam(s^59wx5t=jy8a_)m8dyp8ak7z7-u278sp')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
